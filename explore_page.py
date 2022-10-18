f501393741eb31a919ce5fb8155f9029b89b9667
ethnlpin <eepin.lee@gmail.com>
2022-10-18 20:13:57 +0800

    test again

--- explore_page.py
+++ explore_page.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 import pickle
 
 # Load the model
--- predict_page.py
+++ predict_page.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import numpy as np
-import sklearn
 from sklearn.linear_model import LogisticRegression
 
 # Load the model
