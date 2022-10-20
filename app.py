5853229549c74bf9b461977fb8a04f927e76af63
ethnlpin <eepin.lee@gmail.com>
2022-10-20 19:29:37 +0800

    upload ppt

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
-page = st.sidebar.selectbox("PUNTERS WELCOME!", ["Any tips to bet on horses?", "Predict your horse now!"])
+page = st.sidebar.selectbox("PUNTERS WELCOME!", ["", "Any tips to bet on horses?"])
 
-if page == 'Any tips to bet on horses?':
-    show_explore_page()
-else:
+if page == 'Predict your horse now!':
     show_predict_page()
+else:
+    show_explore_page()
